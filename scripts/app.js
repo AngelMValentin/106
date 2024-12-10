6819ad0c150e41aacf4f20ca61c5abff66213bf7
AngelMValentin <angelvalentinusmc@gmail.com>
2024-12-09 17:55:49 -0800

    Assignment 3

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,66 @@
 function saveTask() {
     console.log("saving task");
+
+    // get values 
+
+    const title = $("#txtTitle").val();
+    const description = $("#txtDescription").val();
+    const color = $("#txtColor").val();
+    const date = $("#txtDate").val();
+    const status = $("#txtStatus").val();
+    const budget = $("#txtBudget").val();
+    console.log(title, description, color, date, status, budget);
+
+    // build an object 
+
+    let taskToSave = new Task(title, description, color, date, status, budget);
+    console.log(taskToSave);
+
+    // save to server 
+    $.ajax({
+        type: "POST",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+        data: JSON.stringify(taskToSave),
+        contentType: "application/json",
+        success: function(response) {
+            console.log(response);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+    // display the task
+    displayTask(taskToSave);
+}
+
+function displayTask(task) {
+
+    console.log(task);
+    let syntax = `<div class="task">
+    <h5>${task.title}</h5>
+    <p>${task.description}</p>
+        </div>
+        <div><label>${task.status}</label></div>
+        <div><label>${task.date}</label>
+        <label>${task.budget}</label></div>`
+        ;
+
+    $(".list").append(syntax);
+    
+}
+
+function testRequest() {
+    $.ajax({
+        type: "get",
+        url: "http://fsdiapi.azurewebsites.net",
+        success: function(response){
+            console.log(response);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
 }
 
 
